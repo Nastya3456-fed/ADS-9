06321a24e6fa41b1c8ea14f85d7b5ee0e2c3e7d8
Nastya3456-fed <Nasty3256@yandex.ru>
2025-06-03 23:16:57 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -6,25 +6,24 @@
 #include <memory>
 
 class PMTree {
-public:
-    struct Node {
-        char value;
-        std::vector<std::unique_ptr<Node>> children;
+ public:
+  struct Node {
+    char value;
+    std::vector<std::unique_ptr<Node>> children;
 
-        Node(char val) : value(val) {}
-    };
+    Node(char val) : value(val) {}
+  };
+  PMTree(const std::vector<char>& elements);
+  ~PMTree() = default;
 
-    PMTree(const std::vector<char>& elements);
-    ~PMTree() = default;
+  const Node* getRoot() const { return root.get(); }
+  size_t getTotalPermutations() const { return total_permutations; }
 
-    const Node* getRoot() const { return root.get(); }
-    size_t getTotalPermutations() const { return total_permutations; }
+ private:
+   std::unique_ptr<Node> root;
+   size_t total_permutations;
 
-private:
-    std::unique_ptr<Node> root;
-    size_t total_permutations;
-
-    void buildTree(Node* parent, const std::vector<char>& remaining_elements);
+   void buildTree(Node* parent, const std::vector<char>& remaining_elements);
 };
 
 std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
